b7fde8a5ab6edd82b674215b00aafc47aece915c
Gorizory <sobenin.g@gmail.com>
2019-12-25 08:52:08 +0300

    Revert "4-ray alg"
    This reverts commit 6365504f

--- types.ts
+++ types.ts
@@ -30,8 +30,6 @@ export type Polygon = {
 export type Ray = {
     directionPriority: RayDirection[],
     path: Point[],
-    isSecondary: boolean,
-    isStopped: boolean,
 };
 
 export type Input = {
